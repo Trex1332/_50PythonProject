4c948b1d19142fa7d008412d7ce342204e679242
Sameul Jamieson <samueljamieson02@gmail.com>
2025-09-07 20:34:32 +0100

    gets all the words used and sees how many are used

--- project3words/words.py
+++ project3words/words.py
@@ -3,4 +3,17 @@
 paragraph = open("words.txt")
 words = paragraph.read()
 amountOfWords = len(words.split())
+word = words.split()
+
+dict = {}
+
+for i in word:
+    if "." in i:
+        i = i.replace(".","")
+    if i in dict:
+        dict[i] +=1
+    else:
+        dict.update({i:1})
+
+print(dict)
 print(amountOfWords)
