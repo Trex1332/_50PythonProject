dd4cf031457999198dbab3849b874703c64b17be
Sameul Jamieson <samueljamieson02@gmail.com>
2025-09-15 20:50:40 +0100

    gets rid of new line at end of word

--- project6hangman/hangman.py
+++ project6hangman/hangman.py
@@ -1,5 +1,17 @@
+import random
 def getword():
+    #open file
     file = open("word.txt")
-    print(file.read())
+    #each line into a list
+    words = file.readlines()
+
+    #need to remove newline
+    for i in range(len(words)):
+        words[i] = words[i].rstrip()
+    #gets a random word
+    word = random.choice(words)
+    #print word
+    print(word)
+    print(len(word))
 
 getword()
