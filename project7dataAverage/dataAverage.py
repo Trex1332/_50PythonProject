aae9f919a61e7c4017fc472906b48eefc66f6ca7
Sameul Jamieson <samueljamieson02@gmail.com>
2025-09-19 20:41:24 +0100

    made first data analysis stuff

--- project7dataAverage/dataAverage.py
+++ project7dataAverage/dataAverage.py
@@ -1,5 +1,11 @@
-#get data from file
+import pandas as pd
+##get data from file
+df = pd.read_csv('car_sales_data.csv')
 
-#look through data
+#print(df.head())
 
+#average price of cars 
+df['Car'] = df['Manufacturer'] + " " + df['Model'] + " " + df['Price'].astype(str)
+averagecars = df['Car'].value_counts().head(3)
+print(averagecars)
 #get average
